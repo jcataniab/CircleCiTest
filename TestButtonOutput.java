644e5dacb72e6f97e06a327aa0cac7bc1b069246
jcataniab <49826636+jcataniab@users.noreply.github.com>
2019-04-21 13:40:29 -0700

    Update TestButtonOutput.java

--- TestButtonOutput.java
+++ TestButtonOutput.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestButtonOutput {
     public static void main(String[] args) {
